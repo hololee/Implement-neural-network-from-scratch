e271068144761112973a4e7bf0cac3c8007f0f8d
hololee <lccandol@naver.com>
2019-10-15 13:24:03 +0900

    update

--- question-a.py
+++ question-a.py
@@ -1,17 +1,46 @@
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
 
-# data stack for
+# data stack for ploting.
 train_acc = []
 train_err = []
 valid_acc = []
 valid_err = []
 
+
+# plotting func.
+def plotting(train_acc, train_err, valdiate_acc, validate_err):
+    train_acc = np.array(train_acc)
+    train_err = np.array(train_err)
+    valdiate_acc = np.array(valdiate_acc)
+    validate_err = np.array(validate_err)
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+
+    ax1.set_xlabel("accuracy")
+    ax1.set_ylim([0, 1])
+    ax1.plot(train_acc, "r", label='train')
+    ax1.plot(valdiate_acc, "g", label='test')
+    ax1.legend()
+
+    ax2.set_xlabel("loss")
+    ax2.set_ylim([0, 0.01])
+    ax2.plot(train_err, "r",label='train')
+    ax2.plot(validate_err, "g",label='test')
+    ax1.legend()
+
+    plt.show()
+
+
 # Network model class.
 class NeuralNetwork:
     # config data.
-    TOTAL_EPOCH = 40
+    TOTAL_EPOCH = 60
     BATCH_SIZE = 1000
     LEARNING_RATE = 0.001
     SEED = 42
@@ -121,12 +150,35 @@ for i in range(NeuralNetwork.TOTAL_EPOCH):
         # TODO: back-propagation term.
         dW1, dW2, dW3 = network_model.backpropagation_sigmoid(batch_x, batch_y, y1, y2, y3)
         network_model.update_weight(dW1, dW2, dW3)
+
     print("============== EPOCH {} END ================".format(i + 1))
 
-    # shake data.
+    # shake data when epoch ended.
     network_model.shake_data()
 
-    y1, y2, y3 = network_model.feedForward_sigmoid(network_model.X_test)
-    correct_prediction = np.equal(np.argmax(y3, axis=1), np.argmax(network_model.y_test, axis=1))
-    accuracy = np.mean(correct_prediction)
-    print(accuracy)
+    # add one epoch data. just using 10000 data.
+    # because all dataset is too big.
+    _, _, y_train3 = network_model.feedForward_sigmoid(network_model.X_train[:NeuralNetwork.TEST_DATASET_SIZE])
+    match_prediction_train = np.equal(np.argmax(y_train3, axis=1),
+                                      np.argmax(network_model.y_train[:NeuralNetwork.TEST_DATASET_SIZE], axis=1))
+    accuracy_train = np.mean(match_prediction_train)
+    match_loss_train = y_train3 - network_model.y_train[:NeuralNetwork.TEST_DATASET_SIZE]
+
+    train_acc.append(accuracy_train)
+    train_err.append(np.mean(match_loss_train))
+
+    # calculate test dataset.
+    _, _, y_test3 = network_model.feedForward_sigmoid(network_model.X_test)
+    match_prediction_test = np.equal(np.argmax(y_test3, axis=1), np.argmax(network_model.y_test, axis=1))
+    accuracy_test = np.mean(match_prediction_test)
+    match_loss_test = y_test3 - network_model.y_test
+
+    valid_acc.append(accuracy_test)
+    valid_err.append(np.mean(match_loss_test))
+    print("train accuracy : {:.4}; loss : {:.3}, test accuracy : {:.3}; loss : {:.3}".format(accuracy_train,
+                                                                                             np.mean(match_loss_train),
+                                                                                             accuracy_test,
+                                                                                             np.mean(match_loss_test)))
+
+    # draw graph.
+    plotting(train_acc, train_err, valid_acc, valid_err)
