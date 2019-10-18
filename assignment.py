6da96324182219795739687898f7120b08d9973f
hololee <lccandol@naver.com>
2019-10-18 10:54:52 +0900

    update readme.

--- assignment.py
+++ assignment.py
@@ -12,36 +12,36 @@ valid_acc = []
 valid_err = []
 
 # set config.
-config_assignmentA = {'total_epoch': 120, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
+config_assignmentA = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
                       'train_dataset_size': 60000, 'test_dataset_size': 10000, 'optimizer': nn.model.OPTIMIZER_GD,
                       'activation': nn.model.ACTIVATE_SIGMOID}
 
-config_assignmentB = {'total_epoch': 120, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
+config_assignmentB = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
                       'train_dataset_size': 60000, 'test_dataset_size': 10000, 'optimizer': nn.model.OPTIMIZER_GD,
                       'activation': nn.model.ACTIVATE_RELU}
 
-config_assignmentC_MINI_BATCH = {'total_epoch': 120, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
+config_assignmentC_MINI_BATCH = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
                                  'train_dataset_size': 60000, 'test_dataset_size': 10000,
                                  'optimizer': nn.model.OPTIMIZER_GD,
                                  'activation': nn.model.ACTIVATE_RELU}
 
-config_assignmentC_BATCH = {'total_epoch': 120, 'batch_size': 60000, 'learning_rate': 0.1, 'random_seed': 42,
+config_assignmentC_BATCH = {'total_epoch': 80, 'batch_size': 60000, 'learning_rate': 0.1, 'random_seed': 42,
                             'train_dataset_size': 60000, 'test_dataset_size': 10000,
                             'optimizer': nn.model.OPTIMIZER_GD,
                             'activation': nn.model.ACTIVATE_RELU}
 
-config_assignmentC_STOCHASTIC = {'total_epoch': 120, 'batch_size': 1, 'learning_rate': 0.01, 'random_seed': 42,
+config_assignmentC_STOCHASTIC = {'total_epoch': 10, 'batch_size': 1, 'learning_rate': 0.01, 'random_seed': 42,
                                  'train_dataset_size': 60000, 'test_dataset_size': 10000,
                                  'optimizer': nn.model.OPTIMIZER_GD,
                                  'activation': nn.model.ACTIVATE_RELU}
 
-config_assignmentD_ADAGRAD = {'total_epoch': 120, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
+config_assignmentD_ADAGRAD = {'total_epoch': 30, 'batch_size': 1000, 'learning_rate': 0.005, 'random_seed': 42,
                               'train_dataset_size': 60000, 'test_dataset_size': 10000,
                               'optimizer': nn.model.OPTIMIZER_ADAGRAD,
                               'activation': nn.model.ACTIVATE_RELU,
                               'epsilon': 1e-5}
 
-config_assignmentD_ADAM = {'total_epoch': 120, 'batch_size': 1000, 'learning_rate': 0.01, 'random_seed': 42,
+config_assignmentD_ADAM = {'total_epoch': 30, 'batch_size': 1000, 'learning_rate': 0.005, 'random_seed': 42,
                            'train_dataset_size': 60000, 'test_dataset_size': 10000,
                            'optimizer': nn.model.OPTIMIZER_ADAM,
                            'activation': nn.model.ACTIVATE_RELU,
